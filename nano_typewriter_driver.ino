e8fe08e8d1088620dee91d5cf32c6d3c8b56b459
halfburnttoast <halfburnttoast@users.noreply.github.com>
2020-09-01 18:56:00 -0400

    Forgot to remove debug code

--- nano_typewriter_driver.ino
+++ nano_typewriter_driver.ino
@@ -253,10 +253,7 @@ void loop() {
     if(triggered == 0 && (PINB & 0x10)) {
         triggered = 1;
         uint8_t bytein = ((PIND & 0xE0) >> 5);
-        sprint("D: %02X, B: %02X\n", PIND, PINB);
-        sprint("%02X -> ", bytein);
         bytein |= ((PINB & 0x1F) << 3);
-        sprint("%02X\n", bytein);
         rb_write((bytein & 0x7F));
     } 
     else if(triggered == 1 && !(PINB & 0x10)) {
